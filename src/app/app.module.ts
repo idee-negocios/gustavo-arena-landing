a18740a1646d1d3486c578f34ca897ceb6a525c8
Alan <alan.boglioli@gmail.com>
2017-04-26 19:31:27 -0300

    Add main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,26 @@ import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AwardComponent } from './header/award/award.component';
+import { TrajectoryComponent } from './trajectory/trajectory.component';
+import { BalanceComponent } from './balance/balance.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { ManagementComponent } from './management/management.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AwardComponent,
+    TrajectoryComponent,
+    BalanceComponent,
+    StatisticsComponent,
+    ManagementComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/trajectory/trajectory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ga-trajectory',
+  templateUrl: './trajectory.component.html',
+  styleUrls: ['./trajectory.component.scss']
+})
+export class TrajectoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
